b2bd5d39656d960efebfcbc401c0571ad3623b15
Kevin Haberl <kzumelvin@github.com>
2026-06-08 19:29:03 +0200

    turniercard

--- src/app/turniere/page.tsx
+++ src/app/turniere/page.tsx
@@ -18,7 +18,7 @@ async function page() {
     <div className='flex flex-row 2xl:grid 2xl:grid-cols-6 justify-center items-center gap-4 flex-wrap w-full p-4'>
       {turniere.map(t => (
         <Link key={t.id} href={`/turniere/${t.id}`}>
-          <Card className='basis-1 aspect-video overflow-clip'>
+          <Card className='w-80 aspect-video overflow-hidden'>
             <CardHeader>
               <CardTitle className='text-4xl'>{t.tournament_name}</CardTitle>
             </CardHeader>
@@ -26,7 +26,7 @@ async function page() {
               <div className="line-clamp-1 flex gap-2 font-medium">
                 Champions
               </div>
-              <div className="text-muted-foreground text-nowrap">
+              <div className="text-muted-foreground">
                 {t.expand.p1 ? t.expand.p1.team_name : "Noch nicht bekannt"}
               </div>
             </CardFooter>
